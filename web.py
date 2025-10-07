1806feb8acedd6c31c4c03900608c5ce4d37e34e
aradhyanand20 <aradhyanand24k@gmail.com>
2025-10-07 23:00:21 +0530

    complete feature

--- web.py
+++ web.py
@@ -10,7 +10,13 @@ st.title("TODO APP")
 st.subheader("Enter your tasks below")
 st.write("Aim is to increase productivity")
 
-for todo in todo_web:
-    st.checkbox(todo)
+for index,todo in enumerate(todo_web):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todo_web.pop(index)
+        functions.write_todos(todo_web)
+        del st.session_state[todo]
+        st.rerun()
 st.text_input(label="", placeholder="Enter your task",
               on_change=add_todos, key='newtodo')
+
